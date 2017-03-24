a6cf6a6c666f31ffa96653621ca5b55ab2f47fa2
yumkong <yumkongleo@gmail.com>
2017-03-23 22:40:14 -0400

    add cls_anno layer for single cls loss ohem

--- include/caffe/rfcn_layers.hpp
+++ include/caffe/rfcn_layers.hpp
@@ -102,6 +102,46 @@ namespace caffe {
   };
 
   /**
+	 * BoxAnnotatorOHEMLayer:
+	 *	 Annotate box labels for Online Hard Example Mining (OHEM) training
+	 */
+	template <typename Dtype>
+	class ClfAnnotatorOHEMLayer :public Layer<Dtype>{
+	public:
+	  explicit ClfAnnotatorOHEMLayer(const LayerParameter& param)
+		: Layer<Dtype>(param) {}
+	  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+		const vector<Blob<Dtype>*>& top);
+	  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+		const vector<Blob<Dtype>*>& top);
+  
+	  virtual inline const char* type() const { return "ClfAnnotatorOHEM"; }
+  
+	  virtual inline int ExactNumBottomBlobs() const { return 3; }
+	  virtual inline int ExactNumTopBlobs() const { return 1; }
+  
+	protected:
+	  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+		const vector<Blob<Dtype>*>& top);
+	  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+		const vector<Blob<Dtype>*>& top);
+	  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+		const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+	  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+		const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  
+	  int num_;
+	  int height_;
+	  int width_;
+	  int spatial_dim_;
+	  int bbox_channels_;
+  
+	  int roi_per_img_;
+	  int ignore_label_;
+  	};
+
+
+  /**
    * BoxAnnotatorOHEMLayer:
    *   Annotate box labels for Online Hard Example Mining (OHEM) training
    */
