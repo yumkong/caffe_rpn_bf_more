b126fb60089f0e2aac31c3ddbf2d03eb8e2e794f
yumkong <yumkongleo@gmail.com>
2017-07-18 21:00:13 -0400

    comment log print in l1 loss

--- src/caffe/layers/smooth_L1_loss_layer.cu
+++ src/caffe/layers/smooth_L1_loss_layer.cu
@@ -147,7 +147,7 @@ namespace caffe {
         Dtype pre_fixed_normalizer = this->layer_param_.loss_param().pre_fixed_normalizer();
 
         Dtype normalizer =  get_normalizer(normalization_, pre_fixed_normalizer, valid_count);
-		LOG(INFO) << "SmoothL1 BP: normalizer = " << normalizer <<", normalization_ = " << normalization_<<", pre_fixed_normalizer = " << pre_fixed_normalizer;
+		//LOG(INFO) << "SmoothL1 BP: normalizer = " << normalizer <<", normalization_ = " << normalization_<<", pre_fixed_normalizer = " << pre_fixed_normalizer;
         Dtype alpha = sign * top[0]->cpu_diff()[0] / normalizer;
 
         caffe_gpu_axpby(
